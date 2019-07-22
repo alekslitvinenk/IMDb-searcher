06d5e6b7a83fdd1b8a4e5a81ba4fda253872a57a
Aleksandr Litvinenko <alekslitvinenk@gmail.com>
2019-07-22 23:45:09 +0300

    Cleanup

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --privileged \
--v /var/run/docker.sock:/var/run/docker.sock \
--e DOCKER_USER="alekslitvinenk" \
--e DOCKER_PASSWORD="Ichufef@17" \
--it alekslitvinenk/imdb bash
